7302e3e9f9e147bf1453d0fa1ea1c54df199ddb2
Myro Joy Lee <myro.joy.olida.092282@gmail.com>
2023-06-16 16:21:53 -0400

    completed seed files

--- seeds/index.js
+++ seeds/index.js
@@ -1,14 +1,23 @@
 const sequelize = require("../config/connection");
-const Post = require("../models/Post");
-const postData = require("./post-seeds.json");
+const { Blogger, Post } = require("../models");
+
+const bloggerData = require("./bloggerData.json");
+const postData = require("./postData.json");
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
 
-  await Post.bulkCreate(postData, {
+  const bloggers = await Blogger.bulkCreate(bloggerData, {
     individualHooks: true,
     returning: true,
   });
+
+  for (const post of postData) {
+    await Post.create({
+      ...post,
+      blogger_id: bloggers[Math.floor(Math.random() * bloggers.length)].id,
+    });
+  }
 
   process.exit(0);
 };
