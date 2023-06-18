7d8c88dda2bc9cc4b1a71674b03f23223952f62e
Myro Joy Lee <myro.joy.olida.092282@gmail.com>
2023-06-18 03:34:05 -0400

    commented out comment route

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,10 +1,10 @@
 const router = require("express").Router();
 const bloggerRoutes = require("./bloggerRoutes");
 const postRoutes = require("./postRoutes");
-const commentRoutes = require("./commentRoutes");
+// const commentRoutes = require("./commentRoutes");
 
 router.use("/bloggers", bloggerRoutes);
 router.use("/posts", postRoutes);
-router.use("/comments", commentRoutes);
+// router.use("/comments", commentRoutes);
 
 module.exports = router;
